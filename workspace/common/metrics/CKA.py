f29916cc2378928c557aaae4e39b40ee1b2fd76f
tom <balditommaso22@gmail.com>
2023-11-26 23:45:45 +0000

    working on CKA implementation

--- workspace/common/metrics/CKA.py
+++ workspace/common/metrics/CKA.py
@@ -1,33 +1,21 @@
+
+"""The following code is adapted from 
+Similarity of Neural Network Representations Revisited
+Simon Kornblith, Mohammad Norouzi, Honglak Lee and Geoffrey Hinton
+https://colab.research.google.com/github/google-research/google-research/blob/master/representation_similarity/Demo.ipynb
+"""
 from __future__ import print_function
 
 import os 
 import sys
+from metric import Metric
+
 import numpy as np
 import pickle
 import torch.nn as nn
 import torch.nn.functional as F
 
-from arguments import get_parser
-from utils import *
 from workspace.common.metrics.utils.CKA_utils import *
-
-# get the parsed arguments from 'arguments.py'
-parser = get_parser(code_type='CKA')
-args = parser.parse_args()
-
-# get the model architecture picked from the user
-model_arch = args.arch.split('_')[0]
-print('Importing code for', model_arch)
-# set the path to import the proper data loader and model
-if model_arch == 'JT':
-    sys.path.append(os.path.join(sys.path[0], "../jets/code")) 
-elif model_arch == 'ECON':
-    sys.path.append(os.path.join(sys.path[0], "../econ/code")) 
-
-# Import dataloader & model 
-from data import get_loader
-from model import load_checkpoint
-
 
 # Get data
 train_loader, test_loader = get_loader(args)
@@ -102,4 +90,115 @@ for exp_id1 in range(3):
         f.close()
         
         
+class CKA(Metric):
+    def __init__(self, model=None, data_loader=None, name="CKA_similarity", loss=None):
+        super().__init__(model, data_loader, name)
+        self.loss = loss
+        self.results = {}   # there will be different values
+    
+    """Compute Gram (kernel) matrix for a linear kernel.
+    Args:
+        x: A num_examples x num_features matrix of features.
+    Returns:
+        A num_examples x num_examples Gram matrix of examples.
+    """
+    def _gram_linear(self, x):
+        return x.dot(x.T)
+
+    """Compute Gram (kernel) matrix for an RBF kernel.
+    Args:
+        x: A num_examples x num_features matrix of features.
+        threshold: Fraction of median Euclidean distance to use as RBF kernel
+        bandwidth. (This is the heuristic we use in the paper. There are other
+        possible ways to set the bandwidth; we didn't try them.)
+    Returns:
+        A num_examples x num_examples Gram matrix of examples.
+    """
+    def _gram_rbf(self, x, threshold=1.0):
+        dot_products = x.dot(x.T)
+        sq_norms = np.diag(dot_products)
+        sq_distances = -2 * dot_products + sq_norms[:, None] + sq_norms[None, :]
+        sq_median_distance = np.median(sq_distances)
         
+        return np.exp(-sq_distances / (2 * threshold ** 2 * sq_median_distance))
+
+    """Center a symmetric Gram matrix.
+    This is equivalent to centering the (possibly infinite-dimensional) features
+    induced by the kernel before computing the Gram matrix.
+    Args:
+        gram: A num_examples x num_examples symmetric matrix.
+        unbiased: Whether to adjust the Gram matrix in order to compute an unbiased
+        estimate of HSIC. Note that this estimator may be negative.
+    Returns:
+        A symmetric matrix with centered columns and rows.
+    """
+    def _center_gram(self, gram, unbiased=False):
+        if not np.allclose(gram, gram.T):
+            raise ValueError('Input must be a symmetric matrix.')
+        gram = gram.copy()
+
+        if unbiased:
+            # This formulation of the U-statistic, from Szekely, G. J., & Rizzo, M.
+            # L. (2014). Partial distance correlation with methods for dissimilarities.
+            # The Annals of Statistics, 42(6), 2382-2412, seems to be more numerically
+            # stable than the alternative from Song et al. (2007).
+            n = gram.shape[0]
+            np.fill_diagonal(gram, 0)
+            means = np.sum(gram, 0, dtype=np.float64) / (n - 2)
+            means -= np.sum(means) / (2 * (n - 1))
+            gram -= means[:, None]
+            gram -= means[None, :]
+            np.fill_diagonal(gram, 0)
+        else:
+            means = np.mean(gram, 0, dtype=np.float64)
+            means -= np.mean(means) / 2
+            gram -= means[:, None]
+            gram -= means[None, :]
+
+        return gram
+    
+    """Helper for computing debiased dot product similarity (i.e. linear HSIC)."""
+    def _debiased_dot_product_similarity_helper(xty, 
+                                                sum_squared_rows_x, 
+                                                sum_squared_rows_y, 
+                                                squared_norm_x, 
+                                                squared_norm_y,
+    n):
+  
+  # This formula can be derived by manipulating the unbiased estimator from
+  # Song et al. (2007).
+  return (
+      xty - n / (n - 2.) * sum_squared_rows_x.dot(sum_squared_rows_y)
+      + squared_norm_x * squared_norm_y / ((n - 1) * (n - 2)))
+
+        
+    """
+    Compute CKA.
+    Args:
+        gram_x: A num_examples x num_examples Gram matrix.
+        gram_y: A num_examples x num_examples Gram matrix.
+        debiased: Use unbiased estimator of HSIC. CKA may still be biased.
+
+    Returns:
+        The value of CKA between X and Y.
+    """
+    def cka(self, gram_X, gram_Y, debiased=True):
+        gram_x = center_gram(gram_x, unbiased=debiased)
+        gram_y = center_gram(gram_y, unbiased=debiased)
+
+        # Note: To obtain HSIC, this should be divided by (n-1)**2 (biased variant) or
+        # n*(n-3) (unbiased variant), but this cancels for CKA.
+        scaled_hsic = gram_x.ravel().dot(gram_y.ravel())
+
+        normalization_x = np.linalg.norm(gram_x)
+        normalization_y = np.linalg.norm(gram_y)
+        return scaled_hsic / (normalization_x * normalization_y)
+        
+    def feature_space_linear_cka(self, gram_X, gram_Y, debiased=True):
+        
+        
+    def compute(self):
+        print("Computing the CKA similarity...")
+        
+        
+        return self.results
