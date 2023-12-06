efc2155019f6d5f383400918c4e987a8b7448c13
balditommaso <balditommaso22@gmail.com>
2023-12-06 12:32:15 -0600

    add missing dependency

--- workspace/models/jets/scripts/jet_dataset_download.sh
+++ workspace/models/jets/scripts/jet_dataset_download.sh
@@ -4,8 +4,8 @@ DATA_DIR=$HOME/loss_landscape_taxonomy/data/JTAG
 
 mkdir -p $DATA_DIR
 
-wget https://zenodo.org/record/3602254/files/hls4ml_LHCjet_100p_train.tar.gz -P $DATA_DIR
-wget https://zenodo.org/record/3602254/files/hls4ml_LHCjet_100p_val.tar.gz -P $DATA_DIR
+wget -nv https://zenodo.org/record/3602254/files/hls4ml_LHCjet_100p_train.tar.gz -P $DATA_DIR
+wget -nv https://zenodo.org/record/3602254/files/hls4ml_LHCjet_100p_val.tar.gz -P $DATA_DIR
 
 tar -zxf $DATA_DIR/hls4ml_LHCjet_100p_train.tar.gz -C $DATA_DIR
 tar -zxf $DATA_DIR/hls4ml_LHCjet_100p_val.tar.gz -C $DATA_DIR
