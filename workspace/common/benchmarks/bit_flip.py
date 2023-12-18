688e31ee10cae941d7f8897c2b78ece35c762890
tom <balditommaso22@gmail.com>
2023-12-18 20:08:25 +0000

    fix bug with bit flip

--- workspace/common/benchmarks/bit_flip.py
+++ workspace/common/benchmarks/bit_flip.py
@@ -6,10 +6,10 @@ import torch
 from statistics import mean
 import numpy as np
 
-module_path = os.path.abspath(os.path.join('../../../workspace/models/econ/code/')) # or the path to your source code
+module_path = os.path.abspath(os.path.join('../../../workspace/models/jets/code/')) # or the path to your source code
 sys.path.insert(0, module_path)
-from q_autoencoder import AutoEncoder
-from autoencoder_datamodule import AutoEncoderDataModule
+from model import JetTagger
+from jet_datamodule import JetDataModule
 
 
 
@@ -50,56 +50,57 @@ class BitFlip:
                     bit_mask = 1 << bit_index
                     target_weight ^= bit_mask
                     flat_weights[weight_index] = target_weight
+                    break
                 else:
                     acc += weights.size
 
 
-# DATA_PATH = '/data/tbaldi/checkpoint/'
-# DATASET_DIR = '../../../data/ECON/Elegun'
-# DATASET_FILE = 'nELinks5.npy'
+# DATA_PATH = '/home/jovyan/checkpoint/'
+# DATASET_DIR = '../../../data/JTAG/'
+# DATASET_FILE = 'processed_dataset.h5'
 
-# def get_model_index_and_relative_EMD(batch_size, learning_rate, precision, size, num_tests=3):
+# def get_model_index_and_relative_accuracy(batch_size, learning_rate, precision, num_tests=5):
 #     '''
 #     Return the average EMDs achieved by the model and the index of best experiment
 #     '''
-#     EMDs = []
-#     min_emd = 1000
-#     min_emd_index = 0
+#     performances = []
+#     max_acc = 0
+#     max_acc_index = 0
 #     for i in range (1, num_tests+1):
 #         file_path = DATA_PATH + f'bs{batch_size}_lr{learning_rate}/' \
-#                     f'ECON_{precision}b/{size}/{size}_emd_{i}.txt'
+#                     f'JTAG_{precision}b/accuracy_{i}.txt'
 #         try:
-#             emd_file = open(file_path)
-#             emd_text = emd_file.read()
-#             emd = ast.literal_eval(emd_text)
-#             emd = emd[0]['AVG_EMD']
-#             EMDs.append(emd)
-#             if min_emd >= emd:
-#                 min_emd = emd
-#                 min_emd_index = i
-#             emd_file.close()
+#             jtag_file = open(file_path)
+#             jtag_text = jtag_file.read()
+#             accuracy = ast.literal_eval(jtag_text)
+#             accuracy = accuracy[0]['test_acc']
+#             performances.append(accuracy)
+#             if accuracy >= max_acc:
+#                 max_acc = accuracy
+#                 max_acc_index = i
+#             jtag_file.close()
 #         except Exception as e:
-#             warnings.warn("Warning: " + file_path + " not found!")
+#             # warnings.warn("Warning: " + file_path + " not found!")
 #             continue
         
-#     if len(EMDs) == 0:
-#         warnings.warn(f"Attention: There is no EMD value for the model: " \
-#                       f"bs{batch_size}_lr{learning_rate}/ECON_{precision}b/{size}")
+#     if len(performances) == 0:
+#         # warnings.warn(f"Attention: There is no accuracy value for the model: " \
+#         #               f"bs{batch_size}_lr{learning_rate}/JTAG_{precision}b")
 #         #TODO: I may compute if the model is there
 #         return
     
-#     return mean(EMDs), min_emd_index
+#     return mean(performances), max_acc_index
 
 
-# def load_model(batch_size, learning_rate, precision, size):
+# def load_model(batch_size, learning_rate, precision):
 #     '''
-#     Method used to get the model and the relative EMD value
+#     Method used to get the model and the relative accuracy
 #     '''
-#     emd, idx = get_model_index_and_relative_EMD(batch_size, learning_rate, precision, size)
-#     model_path = DATA_PATH + f'bs{batch_size}_lr{learning_rate}/ECON_{precision}b/{size}/net_{idx}_best.pkl'
+#     accuracy, idx = get_model_index_and_relative_accuracy(batch_size, learning_rate, precision)
+#     model_path = DATA_PATH + f'bs{batch_size}_lr{learning_rate}/JTAG_{precision}b/net_{idx}_best.pkl'
     
 #     # load the model
-#     model = AutoEncoder(
+#     model = JetTagger(
 #         quantize=(precision < 32),
 #         precision=[
 #             precision,
@@ -107,22 +108,21 @@ class BitFlip:
 #             precision+3
 #         ],
 #         learning_rate=learning_rate,
-#         econ_type=size
 #     )
     
 #     # to set the map location
 #     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     
-#     model(torch.randn((1, 1, 8, 8)))  # Update tensor shapes 
+#     model(torch.randn((16, 16)))  # Update tensor shapes 
 #     model_param = torch.load(model_path, map_location=device)
 #     model.load_state_dict(model_param['state_dict'])
     
-#     return model, emd
+#     return model, accuracy
 
 
 # if __name__ == "__main__":
 #     # get the datamodule
-#     data_module = AutoEncoderDataModule(
+#     data_module = JetDataModule(
 #         data_dir=DATASET_DIR,
 #         data_file=os.path.join(DATASET_DIR, DATASET_FILE),
 #         batch_size=16,
@@ -134,32 +134,27 @@ class BitFlip:
 #         data_module.process_data(save=True)
 
 #     data_module.setup(0)
-    
-#     model, _ = load_model(16, 0.05, 8, 'baseline')
-    
-    
-    
+#     model, _ = load_model(16, 0.05, 8)
 #     model.eval()
     
 #     data_loader = data_module.val_dataloader()
-#     flat1 = model.encoder.enc_dense.weight_integer.detach().numpy().flatten()
+#     flat1 = model.model.dense_1.weight_integer.detach().numpy().flatten()
 #     print(flat1)
 #     # bit flip
-#     bit_flip = BitFlip(model, 8, ['encoder.enc_dense', 'encoder.conv'])
-#     bit_flip.flip_bits(0.01)
+#     bit_flip = BitFlip(model, 8, ['model.dense_1', 'model.dense_2', 'model.dense_3', 'model.dense_4'])
+#     bit_flip.flip_bits(number=10)
 #     count = 0
-#     for batch in data_loader:
+#     for batch, label in data_loader:
 #         count += 1
 #         outout = model(batch)
         
-#         flat2 = model.encoder.enc_dense.weight_integer.detach().numpy().flatten()
+#         flat2 = model.model.dense_1.weight_integer.detach().numpy().flatten()
 #         print(flat2)
 #         print(np.setdiff1d(flat2, flat1))
         
 #         if count == 3:
 #             break
-    # noisy_dataset = NoisyDataset(data_module.dataloaders()[1], 10, 'gaussian')
-    # dataloader = DataLoader(noisy_dataset, batch_size=16, shuffle=True, num_workers=4)
-    # model, _ = load_model(16, 0.05, 8, 'baseline')
-    # bit_flip = BitFlip(model, 8, ['encoder.enc_dense'])
-    # bit_flip.flip_bits(4)
+
+#     model, _ = load_model(16, 0.05, 8)
+#     bit_flip = BitFlip(model, 8, ['encoder.enc_dense'])
+#     bit_flip.flip_bits(10)
