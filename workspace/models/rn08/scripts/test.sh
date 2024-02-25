79790a3ce7a6a5ada74382f7cf9ff4db17d0f4cd
balditommaso <balditommaso22@gmail.com>
2024-02-25 09:54:11 -0600

    special scan

--- workspace/models/rn08/scripts/test.sh
+++ workspace/models/rn08/scripts/test.sh
@@ -15,8 +15,7 @@ num_batches=16
 # default values
 noise_type="pixelate" 
 
-# precisions=(2 3 4 5 6 7 8 9 10 11)
-precisions=(2)
+precisions=(2 3 4 5 6 7 8 9 10 11)
 
 # Function to display script usage
 usage() {
